a375aa1865dd2ef64980011a19a851ffd40c27ea
amelectrovetse09-Sham <amelectrovetse09@gmail.com>
2026-07-18 23:48:28 +0530

    Update courses.js

--- courses.js
+++ courses.js
@@ -1,4 +1,5 @@
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+alert("courses.js loaded");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import {
   getFirestore,
   collection,
